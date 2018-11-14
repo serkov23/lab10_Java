06d5947890931864dced8f5ccce8afc028bf0a87
Sergey Koveleuski <serkov2345@gmail.com>
2018-11-14 20:54:10 +0300

    v.1.0.1.: removed kostyl

--- src/lab10/Interface.java
+++ src/lab10/Interface.java
@@ -8,6 +8,7 @@ public class Interface extends JFrame {
     private static final String STATUS_FORMAT = "x=%d, y=%d";
 
     private JLabel statBar = new JLabel();
+    private JButton button = new JButton();
 
     public Interface() {
         super("I don't know");
@@ -16,9 +17,8 @@ public class Interface extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        var button = new JButton();
-        var workPanel = new JPanel();
 
+        JPanel workPanel = new JPanel();
         workPanel.setLayout(null);
         workPanel.add(button);
         button.setLocation(0, 0);
@@ -28,27 +28,10 @@ public class Interface extends JFrame {
         add(statBar, BorderLayout.SOUTH);
 
         workPanel.addMouseMotionListener(new MouseMotionAdapterForMove());
+        MouseListenerForButton l = new MouseListenerForButton();
+        button.addMouseListener(l);
+        button.addMouseMotionListener(l);
 
-        Point dxy = new Point(0, 0);
-        button.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                dxy.setLocation(e.getPoint());
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                dxy.setLocation(0, 0);
-            }
-        });
-        button.addMouseMotionListener(new MouseMotionAdapterForMove() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                super.mouseDragged(e);
-                if (e.isControlDown())
-                    button.setLocation((int) (e.getX() + e.getComponent().getX() - dxy.getX()), (int) (e.getY() + e.getComponent().getY() - dxy.getY()));
-            }
-        });
 
         button.addKeyListener(new KeyAdapter() {
             @Override
@@ -66,6 +49,42 @@ public class Interface extends JFrame {
                 button.setLocation(e.getX() - button.getWidth() / 2, e.getY() - button.getHeight() / 2);
             }
         });
+    }
+
+    private class MouseListenerForButton extends MouseMotionAdapterForMove implements MouseListener {
+        private Point dxy = new Point(0, 0);
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            super.mouseDragged(e);
+            if (e.isControlDown())
+                button.setLocation((int) (e.getX() + e.getComponent().getX() - dxy.getX()), (int) (e.getY() + e.getComponent().getY() - dxy.getY()));
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            dxy.setLocation(e.getPoint());
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            dxy.setLocation(0, 0);
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 
     private class MouseMotionAdapterForMove extends MouseMotionAdapter {
