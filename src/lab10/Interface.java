d3be1b63619a5204537ee584f93050e836d6f555
Sergey Koveleuski <serkov2345@gmail.com>
2020-10-27 12:25:26 +0300

    init

--- src/lab10/Interface.java
+++ src/lab10/Interface.java
@@ -2,108 +2,110 @@ package lab10;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Interface extends JFrame {
-    private static final String STATUS_FORMAT = "x=%d, y=%d";
-    private static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(500, 250);
-    private static final Dimension MIN_WINDOW_SIZE = new Dimension(50, 100);
-    private static final Dimension DEFAULT_BUTTON_SIZE = new Dimension(70, 25);
-    private static final String DEFAULT_BUTTON_TEXT = "";
 
+    private static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(800, 250);
+    private static final Dimension MIN_WINDOW_SIZE = new Dimension(800, 100);
+    private static final String INPUT_PAT = "[+\\-]?((?:(?:\\d{1,3})(?: ?\\d{3})*)|(?:\\d+))(?:[.,]\\d+)?";
 
-    private JLabel statBar = new JLabel();
-    private JButton button = new JButton();
 
     public Interface() {
-        super("I don't know");
+        super("economy calc");
         this.setSize(DEFAULT_WINDOW_SIZE);
         this.setMinimumSize(MIN_WINDOW_SIZE);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
 
-        JPanel workPanel = new JPanel();
-        workPanel.setLayout(null);
-        workPanel.add(button);
-        button.setLocation(0, 0);
-        button.setSize(DEFAULT_BUTTON_SIZE);
-        button.setText(DEFAULT_BUTTON_TEXT);
-        add(workPanel);
-        add(statBar, BorderLayout.SOUTH);
+        JLabel myInf = new JLabel("Ковалевский Сергей Александрович, 4 курс, 4 группа, 2020");
+        add(myInf, BorderLayout.NORTH);
+        JPanel calcPanel = new JPanel(new BorderLayout());
+        JPanel inPanel = new JPanel(new GridLayout(1, 4));
+        JComboBox<String> operation = new JComboBox<String>(new String[]{"+", "-", "*", "/"});
+        JTextArea oper1 = new JTextArea();
+        oper1.setRows(1);
+        JTextArea oper2 = new JTextArea();
+        JButton calculate = new JButton("=");
+        JTextArea result = new JTextArea();
+        result.setEditable(false);
+        inPanel.add(oper1);
+        inPanel.add(operation);
+        inPanel.add(oper2);
+        inPanel.add(calculate);
+        calcPanel.add(inPanel, BorderLayout.NORTH);
 
-        workPanel.addMouseMotionListener(new MouseMotionAdapterForMove());
+        calcPanel.add(new JLabel("result: "), BorderLayout.WEST);
+        calcPanel.add(result, BorderLayout.CENTER);
 
-        addMouseListenersForButton(button, new MouseListenerForButton());
+        calculate.addActionListener(e -> {
+            String op1 = oper1.getText();
+            String op2 = oper2.getText();
 
-        button.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                if (e.getKeyChar() == '\b')
-                    button.setText(button.getText().length() == 0 ? "" : button.getText().substring(0, button.getText().length() - 1));
-                else if (e.getKeyChar() != '\n')
-                    button.setText(button.getText() + e.getKeyChar());
+            if (op1.matches(INPUT_PAT) && op2.matches(INPUT_PAT)) {
+                BigDecimal decOp1 = new BigDecimal(op1.replace(',', '.').replace(" ", ""));
+                BigDecimal decOp2 = new BigDecimal(op2.replace(',', '.').replace(" ", ""));
+                if (decOp1.compareTo(new BigDecimal("1e12")) > 0 ||
+                        decOp1.compareTo(new BigDecimal("-1e12")) < 0 ||
+                        decOp2.compareTo(new BigDecimal("1e12")) > 0 ||
+                        decOp2.compareTo(new BigDecimal("-1e12")) < 0) {
+                    result.setText("ERROR: Invalid input (operand is out of range)");
+                    return;
+                }
+                if (operation.getSelectedIndex() == 0)
+                    result.setText(process(decOp1.add(decOp2)));
+                else if (operation.getSelectedIndex() == 1)
+                    result.setText(process(decOp1.add(decOp2.negate())));
+                else if (operation.getSelectedIndex() == 2)
+                    result.setText(process(decOp1.multiply(decOp2)));
+                else if (decOp2.equals(new BigDecimal("0")))
+                    result.setText("ERROR: zero division is not allowed");
+                else
+                    result.setText(process(decOp1.divide(decOp2, new MathContext(20, RoundingMode.DOWN))));
+            } else {
+                result.setText("ERROR: Invalid input (invalid operand)");
             }
         });
+        add(calcPanel);
 
-        workPanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                button.setLocation(e.getX() - button.getWidth() / 2, e.getY() - button.getHeight() / 2);
+
+    }
+
+    private String process(BigDecimal res) {
+        if (res.compareTo(new BigDecimal("-1e12")) < 0 ||
+                res.compareTo(new BigDecimal("1e12")) > 0)
+            return "ERROR: Invalid input (result out of bound)";
+        String resStr = res.add(new BigDecimal("5e-7").multiply(new BigDecimal(res.signum()))).toPlainString();
+        int ptPos = resStr.indexOf('.');
+        if (ptPos != -1)
+            try {
+                resStr = resStr.substring(0, ptPos + 7);
+            } catch (Exception ignored) {
             }
-        });
+        int zeroesPosL = resStr.length();
+        for (; ; zeroesPosL--)
+            if (resStr.charAt(zeroesPosL - 1) != '0')
+                break;
+        resStr = resStr.substring(0, zeroesPosL);
+        StringBuilder tmp = new StringBuilder();
+        tmp.append(resStr.charAt(0));
+        for (int i = 1; i < ptPos; i++) {
+            if (i % 3 == ptPos % 3)
+                tmp.append(' ');
+            tmp.append(resStr.charAt(i));
+        }
+        tmp.append(resStr.substring(ptPos));
+
+
+        resStr = tmp.toString();
+        resStr = resStr.replace("- ", "-");
+        if (resStr.charAt(resStr.length() - 1) == '.')
+            resStr = resStr.substring(0, resStr.length() - 1);
+        return resStr;
     }
 
-    private void addMouseListenersForButton(JButton button, MouseListenerForButton l) {
-        button.addMouseListener(l);
-        button.addMouseMotionListener(l);
-    }
 
-    private class MouseListenerForButton extends MouseMotionAdapterForMove implements MouseListener {
-        private Point dxy = new Point(0, 0);
-
-        @Override
-        public void mouseDragged(MouseEvent e) {
-            super.mouseDragged(e);
-            if (e.isControlDown())
-                button.setLocation((int) (e.getX() + e.getComponent().getX() - dxy.getX()), (int) (e.getY() + e.getComponent().getY() - dxy.getY()));
-        }
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-            dxy.setLocation(e.getPoint());
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            dxy.setLocation(0, 0);
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-
-        }
-    }
-
-    private class MouseMotionAdapterForMove extends MouseMotionAdapter {
-        @Override
-        public void mouseMoved(MouseEvent e) {
-            statBar.setText(String.format(STATUS_FORMAT, e.getX() + e.getComponent().getX(), e.getY() + e.getComponent().getY()));
-        }
-
-        @Override
-        public void mouseDragged(MouseEvent e) {
-            mouseMoved(e);
-        }
-    }
 }
